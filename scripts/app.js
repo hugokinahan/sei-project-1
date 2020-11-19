0a323824cfb9151724a33c3b7fd4c0a59775e837
Hugo Kinahan <hugokinahan@hotmail.com>
2020-11-19 20:31:59 +0000

    grid created with indexed divs

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,56 @@
 function init() {
 
-  console.log('Hello')
+  // ! VARIABLES
+  // * Elements 
+  const grid = document.querySelector('.grid')
+  
+  const width = 10
+  const cellCount = width * width
+  const cells = []
+
+
+  // * Make a grid
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // ! EVENT LISTENERS
+
+  // document.addEventListener('keyup')
+
+  createGrid()
 
 }
 
